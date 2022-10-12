dea6533d6122654a55490a0201b1d3d1cc060c87
ITDestr <mattia.abbatangelo00@gmail.com>
2022-10-12 11:28:59 +0200

    Small changes in logger.h and its location I've added colours and small QoL improvement in their usage. Moved logger.h in utils/ instead of /utils/header Removed utils/header folder

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-#include "utils/header/logger.h"
+#include "utils/logger.h"
+
+//Generic qsort
+//https://www.geeksforgeeks.org/generic-implementation-of-quicksort-algorithm-in-c/
 
 int main() {
     printf("Hello, World!\n");
-    LOG_INFO(__FUNCTION__, "CIAO ");
+    LOG_FINE("Hello, i'm a fine log ");
+    LOG_INFO("Hello, i'm a info log");
+    LOG_SEVERE("Hello, i'm a SEVERE log");
+    printf("Hello, i'm a test");
 
     return 0;
 }
