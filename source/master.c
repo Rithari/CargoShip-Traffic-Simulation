65dfc1b92d5c5a64bfb14814955aef1ecf484301
Leoluca D'Atri <22421051+Rithari@users.noreply.github.com>
2022-12-22 14:58:36 +0100

    Restore getpid calls as they get executed only when an error actually occurs

--- source/master.c
+++ source/master.c
@@ -155,22 +155,20 @@ int main(int argc, char **argv) {
 }
 
 void clear_all(void) {
-    pid_t pid;
-    pid = getpid();
 
-    CHECK_ERROR(shmctl(shm_id_config, IPC_RMID, NULL), pid,
+    CHECK_ERROR(shmctl(shm_id_config, IPC_RMID, NULL), getpid(),
                 "[MASTER] Error while removing config shared memory in clear_all")
-    CHECK_ERROR(shmctl(shm_id_goods_template, IPC_RMID, NULL), pid,
+    CHECK_ERROR(shmctl(shm_id_goods_template, IPC_RMID, NULL), getpid(),
                 "[MASTER] Error while removing goods template shared memory in clear_all")
-    CHECK_ERROR(shmctl(shm_id_ports_coords, IPC_RMID, NULL), pid,
+    CHECK_ERROR(shmctl(shm_id_ports_coords, IPC_RMID, NULL), getpid(),
                 "[MASTER] Error while removing ports coordinates shared memory in clear_all")
-    CHECK_ERROR(shmctl(shm_id_pid_array, IPC_RMID, NULL), pid,
+    CHECK_ERROR(shmctl(shm_id_pid_array, IPC_RMID, NULL), getpid(),
                 "[MASTER] Error while removing pid array shared memory in clear_all")
-    CHECK_ERROR(msgctl(mq_id_request, IPC_RMID, NULL), pid,
+    CHECK_ERROR(msgctl(mq_id_request, IPC_RMID, NULL), getpid(),
                 "[MASTER] Error while removing message queue in clear_all")
-    CHECK_ERROR(semctl(sem_id_gen_precedence, 0, IPC_RMID, 0), pid,
+    CHECK_ERROR(semctl(sem_id_gen_precedence, 0, IPC_RMID, 0), getpid(),
                 "[MASTER] Error while removing semaphore for generation order control in clear_all")
-    CHECK_ERROR(semctl(sem_id_docks, 0, IPC_RMID, 0), pid,
+    CHECK_ERROR(semctl(sem_id_docks, 0, IPC_RMID, 0), getpid(),
                 "[MASTER] Error while removing semaphore for docks control in clear_all")
 }
 
